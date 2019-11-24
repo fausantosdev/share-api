25d07e75fcf9bca2a5df7efe0aa44489289e7e7e
Flavio Santos <fall-_santos@hotmail.com>
2019-11-24 12:57:51 -0300

    Added Procfile to heroku

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(
-    "mongodb+srv://fallsantosdev:B13b14b14b13*@cluster-0-dev-4cxi0.mongodb.net/omistack07?retryWrites=true&w=majority",
+    process.env.MONGO_URL,
     { useNewUrlParser: true }
 )
 
